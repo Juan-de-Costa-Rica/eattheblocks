6d63f5b4b88039bd0924ddfac803dd7822c454d1
Juan-de-Costa-Rica <yo@juan-de-costa-rica.com>
2022-02-21 22:50:29 -0600

    7-mocks

---
+++ 6-figure-blockchain-developer/dex-1-smart-contract/7-erc20-token-mocks/mocks/Dai.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.12;
+
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
+
+contract Dai is ERC20 {
+  constructor() ERC20('Dai', 'Dai Stablecoin') {}
+}
