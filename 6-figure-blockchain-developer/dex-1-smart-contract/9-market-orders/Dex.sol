56f9b441f0a1490d81c3f115200b0b3d380cb0b3
Juan-de-Costa-Rica <yo@juan-de-costa-rica.com>
2022-02-22 19:47:57 +0000

    9

--- 6-figure-blockchain-developer/dex-1-smart-contract/9-market-orders/Dex.sol
+++ 6-figure-blockchain-developer/dex-1-smart-contract/9-market-orders/Dex.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity =0.8.12;
+pragma solidity >=0.8.12;
 
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
@@ -16,6 +16,7 @@ contract Dex {
 
     struct Order {
         uint id;
+        address trader;
         Side side;
         bytes32 ticker;
         uint amount;
@@ -30,7 +31,19 @@ contract Dex {
     mapping(bytes32 => mapping(uint => Order[])) public orderBook;
     address public admin;
     uint public nextOrderId;
+    uint public nextTradeId;
     bytes32 constant DAI = bytes32('DAI');
+
+    event NewTrade(
+        uint tradeId,
+        uint orderId,
+        bytes32 indexed ticker,
+        address indexed trader1,
+        address indexed trader2,
+        uint amount,
+        uint price,
+        uint date
+    );
 
     constructor() {
         admin = msg.sender;
@@ -73,8 +86,7 @@ contract Dex {
         uint amount,
         uint price,
         Side side
-    ) external tokenExist(ticker)  {
-        require(ticker != DAI, 'cannot trade DAI');
+    ) external tokenExist(ticker) tokenIsNotDai(ticker) {
         if(side == Side.SELL) {
             require(
                 traderBalances[msg.sender][ticker] >= amount, 
@@ -89,6 +101,7 @@ contract Dex {
         Order[] storage orders = orderBook[ticker][uint(side)];
         orders.push(Order(
             nextOrderId,
+            msg.sender,
             side,
             ticker,
             amount,
@@ -112,6 +125,71 @@ contract Dex {
         }
         nextOrderId++;
     }
+
+    function createMarketOrder(
+        bytes32 ticker,
+        uint amount,
+        Side side
+    ) external tokenExist(ticker) tokenIsNotDai(ticker) {
+        if(side == Side.SELL) {
+            require(
+                traderBalances[msg.sender][ticker] >= amount, 
+                'token balance too low'
+            );
+        }
+        Order[] storage orders = orderBook[ticker][uint(side == Side.BUY ? Side.SELL : Side.BUY)];
+        uint i;
+        uint remaining = amount;
+        
+        while(i < orders.length && remaining > 0) {
+            uint available = orders[i].amount - orders[i].filled;
+            uint matched = (remaining > available) ? available : remaining;
+            remaining -= matched;
+            orders[i].filled += matched;
+            emit NewTrade(
+                nextTradeId,
+                orders[i].id,
+                ticker,
+                orders[i].trader,
+                msg.sender,
+                matched,
+                orders[i].price,
+                block.timestamp
+            );
+            if(side == Side.SELL) {
+                traderBalances[msg.sender][ticker] -= matched;
+                traderBalances[msg.sender][DAI] += matched * orders[i].price;
+                traderBalances[orders[i].trader][ticker] += matched;
+                traderBalances[orders[i].trader][DAI] -= matched * orders[i].price;
+            }
+            if(side == Side.BUY) {
+                require(
+                    traderBalances[msg.sender][DAI] >= matched * orders[i].price,
+                    'dai balance too low'
+                );
+                traderBalances[msg.sender][ticker] += matched;
+                traderBalances[msg.sender][DAI] -= matched * orders[i].price;
+                traderBalances[orders[i].trader][ticker] -= matched;
+                traderBalances[orders[i].trader][DAI] += matched * orders[i].price;
+            }
+            nextTradeId++;
+            i++;
+        }
+        
+        i = 0;
+        while(i < orders.length && orders[i].filled == orders[i].amount) {
+            for(uint j = i; j < orders.length - 1; j++ ) {
+                orders[j] = orders[j + 1];
+            }
+            orders.pop();
+            i++;
+        }
+    }
+
+    modifier tokenIsNotDai(bytes32 ticker) {
+       require(ticker != DAI, 'cannot trade DAI');
+       _;
+    }  
 
     modifier tokenExist(bytes32 ticker) {
         require(
