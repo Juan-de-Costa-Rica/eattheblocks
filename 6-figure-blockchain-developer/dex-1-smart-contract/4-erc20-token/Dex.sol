ff611ab53c876e90fcbe03f25ffcfc8943ca79cc
Juan-de-Costa-Rica <yo@juan-de-costa-rica.com>
2022-02-21 21:54:13 -0600

    6-fig dex

---
+++ 6-figure-blockchain-developer/dex-1-smart-contract/4-erc20-token/Dex.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.12;
+
+
+contract Dex {
+    struct Token {
+        bytes32 ticker;  //? not neccisary?
+        address tokenAddress;
+    }
+
+    mapping(bytes32 => Token) public tokenMap;
+    bytes32[] public tokenList;
+    mapping(address => mapping(bytes32 => uint)) public traderBalances;
+    address public admin;
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    function addToken(
+        bytes32 ticker,
+        address tokenAddress
+    ) external onlyAdmin {
+        tokenMap[ticker] = Token(ticker, tokenAddress);
+        tokenList.push(ticker);
+    }
+
+    modifier onlyAdmin {
+        require(msg.sender == admin, 'only admin');
+        _;
+    }
+}
